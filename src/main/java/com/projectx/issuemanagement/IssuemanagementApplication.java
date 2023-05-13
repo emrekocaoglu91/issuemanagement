65e6265bc9d885ce995f104f310b151843995c8a
emrekocaoglu91 <emrekocaoglu91@gmail.com>
2023-05-13 13:49:23 +0300

    first commit for initializing

---
+++ src/main/java/com/projectx/issuemanagement/IssuemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.projectx.issuemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuemanagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/projectx/issuemanagement/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.projectx.issuemanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello project";
+    }
+
+
+
+}
